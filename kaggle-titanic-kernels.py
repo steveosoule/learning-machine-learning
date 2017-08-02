909d8284992c13dcf9262492b5a2b1b057ff99ec
Steven Soule <me@stevensoule.com>
2017-08-01 22:18:15 -0700

    added kaggle titanic

---
+++ kaggle-titanic-kernels.py
@@ -0,0 +1,4 @@
+# https://www.kaggle.com/c/titanic#tutorials
+
+# https://www.kaggle.com/helgejo/an-interactive-data-science-tutorial
+# https://www.kaggle.com/startupsci/titanic-data-science-solutions
